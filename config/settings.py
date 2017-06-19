55814b08fb09949224cbe6c93cf9bb7974dbfaa7
David Rodgers <dtrodger20@gmail.com>
2017-06-18 20:48:25 -0500

    Initial commit.

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'pickabettersecret'
+
+APP_NAME = 'Moby Dock'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://dave:rene1634@mobydocker.cv6q5bk7olf8.us-east-1.rds.amazonaws.com:5432/mobydock'
+REDIS_URL = 'redis://redis:6379/0'
